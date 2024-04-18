56d19688c6654fbda5677945a2cd54fe17d485ff
jean-claude <jean-claude.grech@orange.fr>
2024-04-18 09:07:48 +0200

    suppression de nav dans sign

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,21 +1,9 @@
-import logo from "./assets/img/argentBankLogo.png";
-import { Link } from "react-router-dom";
+
+
 
 function signIn() {
 	return (
 		<div>
-			<nav className="main-nav">
-				<Link to="/" className="main-nav-logo">
-					<img src={logo} alt="Argent Bank logo" />
-					<h1 className="sr-only">Argent Bank</h1>
-				</Link>
-				<div>
-					<Link to="/signin" className="main-nav-item">
-						<i className="fa fa-user-circle"></i>
-						Sign in
-					</Link>
-				</div>
-			</nav>
 			<main className="main bg-dark">
 				<section className="sign-in-content"></section>
 				<i className="fa fa-user-circle sign-in-icon"></i>
